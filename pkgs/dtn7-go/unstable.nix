61849220822d42c5257fd9e12fd94f35a269182b
update-dtn7-go.sh <nope@nope.local>
2019-12-12 14:49:30 +0000

    Updated dtn7-go

--- pkgs/dtn7-go/unstable.nix
+++ pkgs/dtn7-go/unstable.nix
@@ -2,7 +2,7 @@
 
 callPackage ./. {
   version = "unstable-2019-12-12";
-  rev = "566dc375612caa6de3787ac3ec277888b25e906d";
-  sha256 = "0mdjw0nmcqkxblq3j8hfsp9z58xng5n3113pb36skhrwxry467n2";
+  rev = "11b31396d8404f6360c9dbd01bcc6516a8066781";
+  sha256 = "1d694f25x8ppzqs0zzlb537ja02ndvmrpfz7xq9nqk3xnndcn4xg";
   modSha256 = "0qlnq2xvwzri2hg4z1pjf8h624cq66czql3hwqxpddhc3d7v5mq2";
 }
