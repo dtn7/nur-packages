5bb4adf1e2a5649f857de7d1d1308ec6a9297eb7
Alvar Penning <post@0x21.biz>
2019-12-12 14:01:16 +0100

    update-dtn7-go.sh script to fetch latest versions

---
+++ pkgs/dtn7-go/stable.nix
@@ -0,0 +1,8 @@
+{ callPackage }:
+
+callPackage ./. {
+  version = "0.5.2";
+  rev = "v0.5.2";
+  sha256 = "1i6i98jimp6k3m4b6hh26hwhmm0s0mafmf9584h02bi1z51r6b11";
+  modSha256 = "1rbpq6b1c3pfvrlb53ynfk7d53ylsp90c3m8k3k7wjp0sy982li5";
+}
---
+++ pkgs/dtn7-go/unstable.nix
@@ -0,0 +1,8 @@
+{ callPackage }:
+
+callPackage ./. {
+  version = "unstable-2019-12-12";
+  rev = "4a5f5f8c5fa017c2bbec267f36fc9999e00ef6c0";
+  sha256 = "1hsajf677nphd60p2z6cpbzjz6dv1hf21fl4n9irwyxcyy2iqss6";
+  modSha256 = "0d026p58cmv8i7vdmihrq8dahv8hy5ch45f9sjg8wmwszaz1rhpx";
+}
