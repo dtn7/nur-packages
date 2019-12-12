1f781a6a34a86604eebde04784db9272c564f14e
Alvar Penning <post@0x21.biz>
2019-12-12 16:50:50 +0100

    Installation guide to README

--- pkgs/dtn7-go/unstable.nix
+++ pkgs/dtn7-go/unstable.nix
@@ -2,7 +2,7 @@
 
 callPackage ./. {
   version = "unstable-2019-12-12";
-  rev = "11b31396d8404f6360c9dbd01bcc6516a8066781";
+  rev = "450cff1d15daa08d9bef5fee1820735faa420c8e";
   sha256 = "1d694f25x8ppzqs0zzlb537ja02ndvmrpfz7xq9nqk3xnndcn4xg";
   modSha256 = "0qlnq2xvwzri2hg4z1pjf8h624cq66czql3hwqxpddhc3d7v5mq2";
 }
