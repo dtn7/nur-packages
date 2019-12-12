98e7202b609dcee1bde3350895c7c71e2743d503
update-dtn7-go.sh <nope@nope.local>
2019-12-12 14:29:32 +0000

    Updated dtn7-go

--- pkgs/dtn7-go/stable.nix
+++ pkgs/dtn7-go/stable.nix
@@ -1,8 +1,8 @@
 { callPackage }:
 
 callPackage ./. {
-  version = "0.0.0";
-  rev = "v0.0.0";
-  sha256 = "0000000000000000000000000000000000000000000000000000";
-  modSha256 = "0000000000000000000000000000000000000000000000000000";
+  version = "0.5.2";
+  rev = "v0.5.2";
+  sha256 = "1i6i98jimp6k3m4b6hh26hwhmm0s0mafmf9584h02bi1z51r6b11";
+  modSha256 = "1xcq2hw0ncvsa1chdg19bni731wpva61974qrzv07j4qjw4nhfnm";
 }
