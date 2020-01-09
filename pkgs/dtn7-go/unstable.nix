39a812f1dbf90fe51bf285026747b780fd65c234
update-dtn7-go.sh <>
2020-01-09 21:55:18 +0000

    Updated dtn7-go

--- pkgs/dtn7-go/unstable.nix
+++ pkgs/dtn7-go/unstable.nix
@@ -2,7 +2,7 @@
 
 callPackage ./. {
   version = "unstable-2020-01-09";
-  rev = "3bb2455c97be86a84482e7353012fce63ee21eb1";
-  sha256 = "06p0z3mk8475x9xlhkgd8f9hyvyfjnd8irghz7sl9zdp2q72iggs";
+  rev = "4b2c4e9929dcd8c9f6446b233fd5afa8910d99e4";
+  sha256 = "0fsg33ybad304nvpqfays26x878qzn16z60l93a8h22p9khqfx6b";
   modSha256 = "1dxl5n7k35m0inzfwza3q2hj5z6v68cq0glwy3df150yiv4m55p6";
 }
