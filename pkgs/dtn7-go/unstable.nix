936847a858aae7ca4018eed4f4423833561f4589
update-dtn7-go.sh <>
2021-08-16 18:08:19 +0000

    dtn7-go: automated version bump

--- pkgs/dtn7-go/unstable.nix
+++ pkgs/dtn7-go/unstable.nix
@@ -1,8 +1,8 @@
 { callPackage }:
 
 callPackage ./. {
-  version = "unstable-2021-06-17";
-  rev = "b751261912ce748e068fa8f0bfcb564c30aff6c6";
-  sha256 = "0p837w05ag7svk1v2as7vgc5pqiz62ydp5djwfii4igc5azdaq17";
+  version = "unstable-2021-08-16";
+  rev = "d3b5e62a7f89994ececf98978bae499f32cc920f";
+  sha256 = "19m19c53pkn5l7pvnrc83bbp3fmv0h2kp7wfdn97s53r8lm2a5dq";
   vendorSha256 = "1xf05mb2jkm0w4x426588myxsg4cg5gb7plff71hmpqpwk9vi969";
 }
